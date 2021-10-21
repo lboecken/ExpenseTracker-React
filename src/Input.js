2eb0debd40573a17a92f7db1eb9986006e882655
lennart <lsboecken@gmail.com>
2021-10-21 09:17:18 -0400

    <feat>(<Input.js>): Add form validation

--- src/Input.js
+++ src/Input.js
@@ -1,27 +1,38 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-const InputForm = (props) => {
+function InputForm(props) {
+  const { register, handleSubmit, errors } = useForm();
+
   return (
-    <form onSubmit={props.submitForm}>
+    <form onSubmit={handleSubmit(props.submitForm)}>
       <label>
         Date
-        <input name='date' type='date' onChange={props.updateState} />
+        <input name='date' type='date' ref={register({ required: true })} />
+        {errors.date && <span>This field is required.</span>}
       </label>
       <label>
         Amount:
-        <input name='amount' type='text' onChange={props.updateState} />
+        <input name='amount' type='text' ref={register({ required: true })} />
+        {errors.amount && <span>This field is required.</span>}
       </label>
       <label>
         Vendor:
-        <input name='vendor' type='text' onChange={props.updateState} />
+        <input name='vendor' type='text' ref={register({ required: true })} />
+        {errors.vendor && <span>This field is required.</span>}
       </label>
       <label>
         Comment:
-        <input name='comment' type='text' onChange={props.updateState} />
+        <input name='comment' type='text' ref={register({ required: true })} />
+        {errors.comment && <span>This field is required.</span>}
       </label>
-      <input type='submit' value='Track New Expense' />
+      <input
+        type='submit'
+        value='Track New Expense'
+        ref={register({ required: true })}
+      />
     </form>
   );
-};
+}
 
 export default InputForm;
