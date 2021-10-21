d9da656b3851b0ef583bae702a93985bedf70e77
lennart <lsboecken@gmail.com>
2021-10-21 08:32:29 -0400

    refactor: class components to functional components with react
    	Converted all class components to functional components to
    more easily integrate react-forms-hook.

--- src/App.js
+++ src/App.js
@@ -1,97 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Table from './Table';
 import InputForm from './Input';
 import { nanoid } from 'nanoid';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      Date: '',
-      Amount: '',
-      Vendor: '',
-      Comment: '',
-      currentExpenses: this.populateFromLocalStorage(),
-    };
+const App = () => {
+  const [formValues, setFormValues] = useState({
+    date: '',
+    amount: '',
+    vendor: '',
+    comment: '',
+  });
+  const [currentExpenses, setCurrentExpenses] = useState(
+    populateFromLocalStorage()
+  );
 
-    this.handleFormChange = this.handleFormChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    this.deleteExpense = this.deleteExpense.bind(this);
-    this.deleteExpenseFromLocalStorage =
-      this.deleteExpenseFromLocalStorage.bind(this);
-    this.addExpenseToLocalStorage = this.addExpenseToLocalStorage.bind(this);
-    this.populateFromLocalStorage = this.populateFromLocalStorage.bind(this);
-  }
-
-  handleFormChange(event) {
-    const name = event.target.name;
-    this.setState({ [name]: event.target.value });
-  }
-
-  handleFormSubmit(event) {
-    event.preventDefault();
-    const uniqueExpenseID = nanoid();
-    const newExpense = [
-      this.state.Date,
-      this.state.Amount,
-      this.state.Vendor,
-      this.state.Comment,
-      uniqueExpenseID,
-    ];
-    this.setState(() => {
-      return {
-        currentExpenses: [...this.state.currentExpenses, newExpense],
-      };
-    });
-    this.addExpenseToLocalStorage(newExpense);
-  }
-
-  deleteExpense(event) {
-    event.preventDefault();
-    const deletedExpenseIndex = this.state.currentExpenses.findIndex(
-      (element) => element[4] === event.target['id']
-    );
-    this.deleteExpenseFromLocalStorage(
-      this.state.currentExpenses[deletedExpenseIndex][4]
-    );
-    this.setState({
-      currentExpense: this.state.currentExpenses.splice(deletedExpenseIndex, 1),
-    });
-  }
-
-  deleteExpenseFromLocalStorage(expenseKey) {
-    localStorage.removeItem(expenseKey);
-  }
-
-  populateFromLocalStorage() {
+  function populateFromLocalStorage() {
     const expenseList = [];
+    // could try array.forEach
     for (let i = 0; i < localStorage.length; i++) {
       expenseList.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
     }
     return expenseList;
   }
-
-  addExpenseToLocalStorage(expense) {
-    const JSONexpense = JSON.stringify(expense);
-    localStorage.setItem(expense[4], JSONexpense);
+  function deleteExpenseFromLocalStorage(expenseKey) {
+    localStorage.removeItem(expenseKey);
+  }
+  function addExpenseToLocalStorage(expense) {
+    localStorage.setItem(expense[4], JSON.stringify(expense));
   }
 
-  render() {
-    return (
-      <div id='app'>
-        <InputForm
-          updateState={this.handleFormChange}
-          submitForm={this.handleFormSubmit}
-        />
-        <Table
-          currentExpenses={this.state.currentExpenses}
-          deleteExpense={this.deleteExpense}
-        />
-      </div>
+  function handleFormChange(event) {
+    event.preventDefault();
+    setFormValues({ ...formValues, [event.target.name]: event.target.value });
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const newExpense = [
+      formValues.date,
+      formValues.amount,
+      formValues.vendor,
+      formValues.comment,
+      nanoid(),
+    ];
+    setCurrentExpenses([...currentExpenses, newExpense]);
+    addExpenseToLocalStorage(newExpense);
+  }
+
+  function deleteExpense(event) {
+    event.preventDefault();
+    const deletedExpenseIndex = currentExpenses.findIndex(
+      (element) => element[4] === event.target['id']
     );
+    console.log(deletedExpenseIndex);
+    deleteExpenseFromLocalStorage(currentExpenses[deletedExpenseIndex][4]);
+
+    currentExpenses.splice(deletedExpenseIndex, 1);
+    setCurrentExpenses([...currentExpenses]);
   }
-}
+
+  return (
+    <div id='app'>
+      <InputForm updateState={handleFormChange} submitForm={handleFormSubmit} />
+      <Table currentExpenses={currentExpenses} deleteExpense={deleteExpense} />
+    </div>
+  );
+};
 
 export default App;
-
-//copy currentExpenses to local storage whenever currentExpenses is updated.
--- src/Input.js
+++ src/Input.js
@@ -1,29 +1,27 @@
 import React from 'react';
 
-class InputForm extends React.Component {
-  render() {
-    return (
-      <form onSubmit={this.props.submitForm}>
-        <label>
-          Date
-          <input name='Date' type='date' onChange={this.props.updateState} />
-        </label>
-        <label>
-          Amount:
-          <input name='Amount' type='text' onChange={this.props.updateState} />
-        </label>
-        <label>
-          Vendor:
-          <input name='Vendor' type='text' onChange={this.props.updateState} />
-        </label>
-        <label>
-          Comment:
-          <input name='Comment' type='text' onChange={this.props.updateState} />
-        </label>
-        <input type='submit' value='Track New Expense' />
-      </form>
-    );
-  }
-}
+const InputForm = (props) => {
+  return (
+    <form onSubmit={props.submitForm}>
+      <label>
+        Date
+        <input name='date' type='date' onChange={props.updateState} />
+      </label>
+      <label>
+        Amount:
+        <input name='amount' type='text' onChange={props.updateState} />
+      </label>
+      <label>
+        Vendor:
+        <input name='vendor' type='text' onChange={props.updateState} />
+      </label>
+      <label>
+        Comment:
+        <input name='comment' type='text' onChange={props.updateState} />
+      </label>
+      <input type='submit' value='Track New Expense' />
+    </form>
+  );
+};
 
 export default InputForm;
